fa1746bbd02e7aac9fefab6777b4680108fcf5ae
ansilrahman777 <rahmanansil777@gmail.com>
2025-04-14 22:56:09 +0400

    nf

--- car_nation/src/pages/NotFound.jsx
+++ car_nation/src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-      <div className="text-center py-10 mt-28">
+      <div className="text-center py-10 mt-40">
         <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
         <p className="mt-4">Sorry, the page you're looking for doesn't exist.</p>
       </div>
