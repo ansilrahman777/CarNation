6378354c907ccac190dfab1ebd047225aceaa635
ANSIL RAHMAN <rahmanansil777@gmail.com>
2025-04-16 23:48:35 +0400

    moving text

--- car_nation/src/components/ui/MovingText.jsx
+++ car_nation/src/components/ui/MovingText.jsx
@@ -2,39 +2,45 @@ import { motion } from "framer-motion";
 
 export default function MovingText() {
   return (
-    <div className="overflow-hidden bg-transparent text-white w-full relative">
-      <div className="w-full">
+    <div className="overflow-hidden bg-transparent text-white w-full relative py-6">
+      <div className="rotate-[-5deg] w-full">
         <div className="flex flex-col gap-2">
           <motion.div
-            className="whitespace-nowrap text-[5vw] font-extrabold uppercase flex"
+            className="whitespace-nowrap text-[8vw] font-extrabold uppercase flex"
             animate={{ x: ["0%", "-100%"] }}
             transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
           >
-            <div className="flex text-white">
+            <div
+              className="flex text-transparent"
+              style={{ WebkitTextStroke: "2px white" }}
+            >
               <span className="mx-0">
-                Car Diagnostic Body Works & Painting Premium Service Fast
-                Recovery
+                Car Diagnostic ✷ Body Works & Painting ✷ Premium Service ✷ Fast
+                Recovery ✷
               </span>
               <span className="mx-0">
-                Car Diagnostic Body Works & Painting Premium Service Fast
-                Recovery
+                Car Diagnostic ✷ Body Works & Painting ✷ Premium Service ✷ Fast
+                Recovery ✷
               </span>
             </div>
           </motion.div>
 
           <motion.div
-            className="whitespace-nowrap text-[5vw] text-red-700 font-extrabold uppercase flex"
+            className="whitespace-nowrap text-[8vw] text-red-700 font-extrabold uppercase flex"
             animate={{ x: ["-100%", "0%"] }}
             transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
           >
-            <div className="flex  text-red-700">
+            <div
+              className="flex  text-transparent"
+              style={{ WebkitTextStroke: "2px #b91c1c" }}
+            >
               <span className="mx-0">
-                Expert Technicians Genuine Parts Fast & Reliable Affordable
-                Prices
+                Expert Technicians ✷ Genuine Parts ✷ Fast & Reliable ✷
+                Affordable Prices ✷
               </span>
               <span className="mx-0">
-                Expert Technicians Genuine Parts Fast & Reliable Affordable
-                Prices
+                Expert Technicians ✷ Genuine Parts ✷ Fast & Reliable ✷
+                Affordable Prices ✷
               </span>
             </div>
           </motion.div>
